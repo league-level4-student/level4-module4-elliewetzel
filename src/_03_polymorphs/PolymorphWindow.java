68653cb77c7cde6c37459c7406587e8fc6c0ee7e
Ellie Wetzel <elliehw2005@gmail.com>
2020-10-18 17:25:30 -0700

    polymorphs(ip)

--- src/_03_polymorphs/PolymorphWindow.java
+++ src/_03_polymorphs/PolymorphWindow.java
@@ -5,51 +5,95 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class PolymorphWindow extends JPanel implements ActionListener{
+public class PolymorphWindow extends JPanel implements ActionListener, MouseListener{
     public static final int WIDTH = 900;
     public static final int HEIGHT = 600;
     
     private JFrame window;
     private Timer timer;
-    
-    Polymorph bluePoly;
+    ArrayList<Polymorph> array = new ArrayList<Polymorph>();
     
     public static void main(String[] args) {
    	 new PolymorphWindow().buildWindow();
     }
     
     public void buildWindow(){
-   	 window = new JFrame("IT'S MORPHIN' TIME!");
+   	 window = new JFrame("Time To Morph!");
    	 window.add(this);
    	 window.getContentPane().setPreferredSize(new Dimension(500, 500));
    	 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    	 window.pack();
    	 window.setVisible(true);
-   	 
-   	 bluePoly = new BluePolymorph(50, 50);
+   	 window.addMouseListener(this);
    	 
    	 timer = new Timer(1000 / 30, this);
    	 timer.start();
+   	 
+   	 array.add(new RedPolymorph(10,10,50,50));
+   	 array.add(new CircleMorph(250,250,50,50));
+   	 array.add(new MouseMorph(0,0,50,50));
+   	 array.add(new BluePolymorph(100,10,50,50));
+   	 array.add(new MessageMorph(200,50,50,50));
     }
     
     public void paintComponent(Graphics g){
     //draw background
    	 g.setColor(Color.LIGHT_GRAY);
    	 g.fillRect(0, 0, 500, 500);
-   	
    	 //draw polymorph
-   	 bluePoly.draw(g);
+   	 for (Polymorph polymorph : array) {
+		polymorph.draw(g);
+	}
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
    	 repaint();
-   	 bluePoly.update();
-   	 
+   	 //update polymorph
+   	 for (Polymorph polymorph : array) {
+		polymorph.update();
+	}
     }
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getX() >= array.get(4).x +10 && arg0.getX() <= array.get(4).x+10 + array.get(4).width
+		   && arg0.getY() >= array.get(4).y+37 && arg0.getY() <= array.get(4).y+37 + array.get(4).height) {
+				JOptionPane.showMessageDialog(null, "I am orange");
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
