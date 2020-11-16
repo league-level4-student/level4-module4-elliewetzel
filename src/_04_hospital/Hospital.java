a2cb3a0c296402766cdd419f38bab0e05e6b1163
Ellie Wetzel <elliehw2005@gmail.com>
2020-11-15 17:56:55 -0800

    hospital(IP)

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -1,5 +1,47 @@
 package _04_hospital;
 
-public class Hospital {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hospital {
+	protected ArrayList<Doctor> doc = new ArrayList<Doctor>();
+	protected ArrayList<Patient> pat = new ArrayList<Patient>();
+	
+	public void addDoctor(Doctor doctor) {
+		// TODO Auto-generated method stub
+			doc.add(doctor);
+	}
+
+	public void addPatient(Patient patient) {
+		// TODO Auto-generated method stub
+			pat.add(patient);
+	}
+
+	
+
+	public List<Doctor> getDoctors() {
+		// TODO Auto-generated method stub
+		return doc;
+	}
+
+	public List<Patient> getPatients() {
+		// TODO Auto-generated method stub
+		return pat;
+	}
+
+	public void assignPatientsToDoctors() throws DoctorFullException {
+		// TODO Auto-generated method stub
+		int patIndex = 0;
+		for (int i = 0; i < doc.size(); i++) {
+			while (patIndex<pat.size()) {
+				if(doc.get(i).getPatients().size()<3) {
+				doc.get(i).assignPaitent(pat.get(patIndex));
+				patIndex++;
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
 }
