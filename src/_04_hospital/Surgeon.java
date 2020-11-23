6a9e9519e9f2f537fecdf6359bfa11df24825389
Ellie Wetzel <elliehw2005@gmail.com>
2020-11-22 17:57:56 -0800

    surgeon (ip)

--- src/_04_hospital/Surgeon.java
+++ src/_04_hospital/Surgeon.java
@@ -1,5 +1,26 @@
 package _04_hospital;
 
-public class Surgeon {
+import java.util.ArrayList;
 
-}
+public class Surgeon extends Doctor {
+	protected ArrayList<Patient> pat = new ArrayList<Patient>();
+	
+	public boolean performsSurgery() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	public boolean makesHouseCalls() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void assignPatient(Patient patient) {
+		// TODO Auto-generated method stub
+		if(pat.size()>3) {
+			System.out.println("Full on patients");
+		}
+		else {
+			pat.add(patient);
+		}
+	}
