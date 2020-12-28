0d6185de32415ab9d7a40e2419a7d0b3af22d027
Ellie Wetzel <elliehw2005@gmail.com>
2020-12-27 17:58:37 -0800

    hospital, hospital test, self check points

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -4,56 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hospital {
-/*	protected ArrayList<Doctor> doc = new ArrayList<Doctor>();
-	protected ArrayList<Patient> pat = new ArrayList<Patient>();
-
-	public void addDoctor(Doctor d) {
-	doc.add(d);
-	}
-	/*public void addDoctor(GeneralPractitioner generalPractitioner) {
-		doc.add(generalPractitioner);
-	}*/
-	
-	/*public void addPatient(Patient patient) {
-		
-			pat.add(patient);
-	}	
-
-	public List<Doctor> getDoctors() {
-		return doc;
-	}
-
-	public List<Patient> getPatients() {
-		return pat;
-	}
-
-	public void assignPatientsToDoctors() throws DoctorFullException {
-		int patIndex = 0;
-		for (int i = 0; i < doc.size(); i++) {
-			while (patIndex<pat.size()) {
-				if(doc.get(i).getPatients().size()<3) {
-				doc.get(i).assignPatient(pat.get(patIndex));
-				patIndex++;
-				}
-				else {
-					break;
-				}
-			}
-		}
-	}
-}*/
-
 ArrayList<Doctor> doc = new ArrayList<Doctor>(); 
 ArrayList<Patient> pat = new ArrayList<Patient>(); 
-public void addDoctor(Doctor d) {
-	doc.add(d);
-}
+
 public List<Doctor> getDoctors() {
 	return doc;
 }
 public List<Patient> getPatients() {
 	return pat;
 }
+public void addDoctor(Doctor d) {
+	doc.add(d);
+	}
 public void addPatient(Patient patient) {
 	pat.add(patient);
 	
